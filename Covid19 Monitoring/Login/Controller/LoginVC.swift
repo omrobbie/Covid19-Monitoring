39f9027eae34eabf115f5b83de0aa2387182e7c2
omrobbie <putu.roby@yahoo.com>
2020-04-04 08:42:17 +0800

    refactor folder structure

--- Covid19 Monitoring/Login/Controller/LoginVC.swift
+++ Covid19 Monitoring/Login/Controller/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  Covid19 Monitoring
 //
 //  Created by omrobbie on 03/04/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
