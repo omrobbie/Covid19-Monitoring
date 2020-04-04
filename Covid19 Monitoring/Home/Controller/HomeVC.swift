30162d2768f051d1372a117da1324d6fc5ff7754
omrobbie <putu.roby@yahoo.com>
2020-04-04 16:15:58 +0800

    prepare home layout

---
+++ Covid19 Monitoring/Home/Controller/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  Covid19 Monitoring
+//
+//  Created by omrobbie on 04/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
