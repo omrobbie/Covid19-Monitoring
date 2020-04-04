0e7b216e7e1ffeb44bc8ae2ed595dc560ddfab8e
omrobbie <putu.roby@yahoo.com>
2020-04-04 16:42:40 +0800

    setup home layout with title

--- Covid19 Monitoring/Home/Controller/HomeVC.swift
+++ Covid19 Monitoring/Home/Controller/HomeVC.swift
@@ -10,7 +10,32 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDelegate()
+        setupRegisterNib()
+    }
+
+    private func setupDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    private func setupRegisterNib() {
+        collectionView.register(UINib(nibName: "TitleCell", bundle: nil), forCellWithReuseIdentifier: "TitleCell")
+    }
+}
+
+extension HomeVC: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TitleCell", for: indexPath)
+        return cell
     }
 }
