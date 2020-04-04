78701f77ec848321b50e1fc7efb60159773ad65d
omrobbie <putu.roby@yahoo.com>
2020-04-04 09:46:33 +0800

    add interaction to login

--- Covid19 Monitoring/Login/Controller/LoginVC.swift
+++ Covid19 Monitoring/Login/Controller/LoginVC.swift
@@ -10,7 +10,46 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtEmailWarning: UILabel!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtPasswordWarning: UILabel!
+    @IBOutlet weak var btnLogin: CustomUIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDelegate()
+    }
+
+    private func setupDelegate() {
+        txtEmail.delegate = self
+        txtPassword.delegate = self
+    }
+
+    private func showWarningWhenTextIsEmpty() {
+        txtEmailWarning.isHidden = !txtEmail.text!.isEmpty
+        txtPasswordWarning.isHidden = !txtPassword.text!.isEmpty
+    }
+
+    @IBAction func btnLoginTapped(_ sender: Any) {
+        showWarningWhenTextIsEmpty()
+
+        guard let email = txtEmail.text,
+            let password = txtPassword.text
+            else {
+                return
+        }
+
+        if email == "putu.roby@yahoo.com" && password == "1234" {
+            print("Login Success!")
+        }
+    }
+}
+
+extension LoginVC: UITextFieldDelegate {
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        showWarningWhenTextIsEmpty()
+        btnLogin.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
     }
 }
