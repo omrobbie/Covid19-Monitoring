ebd6f3951f273ec50d6620133032fdf0573d1ae6
omrobbie <putu.roby@yahoo.com>
2020-04-05 13:23:59 +0800

    design global detail layout with dummy data

--- Covid19 Monitoring/Global/Controller/GlobalDetailVC.swift
+++ Covid19 Monitoring/Global/Controller/GlobalDetailVC.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class GlobalDetailVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     var data: CovidCountryModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDelegate()
         setupUI()
+    }
+
+    private func setupDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     private func setupUI() {
         guard let data = data else {return}
 
         print(data.country)
+    }
+}
+
+extension GlobalDetailVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "GlobalDetailCell")
+        cell.textLabel?.text = "Item"
+        cell.detailTextLabel?.text = "\(indexPath.row)"
+        return cell
     }
 }
