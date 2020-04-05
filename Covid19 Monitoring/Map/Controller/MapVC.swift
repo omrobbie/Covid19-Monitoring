35dcc0d6581d02671a10b8a76e459ebd5aa5f25c
omrobbie <putu.roby@yahoo.com>
2020-04-05 22:07:25 +0800

    add a circle overlay to visualize the spread

--- Covid19 Monitoring/Map/Controller/MapVC.swift
+++ Covid19 Monitoring/Map/Controller/MapVC.swift
@@ -29,15 +29,45 @@ class MapVC: UIViewController {
     private func loadData() {
         ApiService.shared.getDataGlobalPerCountry { (data) in
             data.forEach { (item) in
+                let coordinate = CLLocationCoordinate2D(latitude: item.countryInfo.lat, longitude: item.countryInfo.long)
+
                 let annotation = PointAnnotationModel()
                 annotation.data = item
                 annotation.title = item.country
                 annotation.subtitle = "Jumlah kasus: \(item.cases.toCommaSeperated())"
-                annotation.coordinate = CLLocationCoordinate2D(latitude: item.countryInfo.lat, longitude: item.countryInfo.long)
+                annotation.coordinate = coordinate
                 self.mapView.addAnnotation(annotation)
+
+                let overlay = MKCircle(center: coordinate, radius: self.overlayRadius(cases: item.cases))
+                self.mapView.addOverlay(overlay)
             }
 
             self.activityIndicator.isHidden = true
+        }
+    }
+
+    private func overlayRadius(cases: Int) -> CLLocationDistance {
+        switch cases {
+        case 50000..<250000:
+            return 150000
+        case 17000..<50000:
+            return 125000
+        case 3000..<17000:
+            return 100000
+        case 1600..<3000:
+            return 87500
+        case 800..<1600:
+            return 75000
+        case 400..<800:
+            return 67500
+        case 200..<400:
+            return 50000
+        case 50..<200:
+            return 37500
+        case 10..<50:
+            return 25000
+        default:
+            return 15000
         }
     }
 }
@@ -58,5 +88,14 @@ extension MapVC: MKMapViewDelegate {
         vc.modalPresentationStyle = .popover
         vc.data = annotation.data
         present(vc, animated: true, completion: nil)
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let overlay = overlay as? MKCircle else {return MKOverlayRenderer()}
+        let circleRenderer = MKCircleRenderer(circle: overlay)
+        circleRenderer.fillColor = .systemOrange
+        circleRenderer.strokeColor = .clear
+        circleRenderer.alpha = 0.7
+        return circleRenderer
     }
 }
