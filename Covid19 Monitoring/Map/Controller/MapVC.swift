ed11af0161f7d34788425b894ce27ea9bb11a181
omrobbie <putu.roby@yahoo.com>
2020-04-05 18:13:37 +0800

    show map pin description using annotation

--- Covid19 Monitoring/Map/Controller/MapVC.swift
+++ Covid19 Monitoring/Map/Controller/MapVC.swift
@@ -18,18 +18,36 @@ class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDelegate()
         loadData()
+    }
+
+    private func setupDelegate() {
+        mapView.delegate = self
     }
 
     private func loadData() {
         ApiService.shared.getDataGlobalPerCountry { (data) in
             data.forEach { (item) in
                 let annotation = MKPointAnnotation()
+                annotation.title = item.country
+                annotation.subtitle = "Jumlah kasus: \(item.cases.toCommaSeperated())"
                 annotation.coordinate = CLLocationCoordinate2D(latitude: item.countryInfo.lat, longitude: item.countryInfo.long)
                 self.mapView.addAnnotation(annotation)
             }
 
             self.activityIndicator.isHidden = true
         }
+    }
+}
+
+extension MapVC: MKMapViewDelegate {
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        annotationView.isEnabled = true
+        annotationView.canShowCallout = true
+        annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        return annotationView
     }
 }
